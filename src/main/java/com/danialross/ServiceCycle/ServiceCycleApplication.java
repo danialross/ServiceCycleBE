e5d08b59445a72b649f820ca9b87caf8d2fbae6e
danialross <danialrossar@gmail.com>
2026-05-11 23:58:22 +0800

    init
    add supabase base configs

---
+++ src/main/java/com/danialross/ServiceCycle/ServiceCycleApplication.java
@@ -0,0 +1,13 @@
+package com.danialross.ServiceCycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danialross/ServiceCycle/ServiceCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danialross.ServiceCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
