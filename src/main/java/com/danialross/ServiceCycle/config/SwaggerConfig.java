c2b9c9b93c073edf18092409313081a0646f9ce5
danialross <danialrossar@gmail.com>
2026-05-18 13:29:33 +0800

    feat(Config): add auth button for swagger

---
+++ src/main/java/com/danialross/ServiceCycle/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.danialross.ServiceCycle.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ServiceCycle API")
+                        .version("1.0.0")
+                        .description("Vehicle Service Tracker API"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Token"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("Bearer Token",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
+}
