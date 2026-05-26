2a58a1b831635cb7807808d7f15d87e40212aa7d
danialross <danialrossar@gmail.com>
2026-05-26 22:17:08 +0800

    feat(Mileage Record and Maintenance Record): add entity

--- src/main/java/com/danialross/ServiceCycle/modules/MaintenanceRecord/MaintenanceRecord.java
+++ src/main/java/com/danialross/ServiceCycle/modules/MaintenanceRecord/MaintenanceRecord.java
@@ -1,14 +1,23 @@
 package com.danialross.ServiceCycle.modules.MaintenanceRecord;
 
+import com.danialross.ServiceCycle.modules.parts.Part;
 import com.danialross.ServiceCycle.modules.vehicles.Vehicle;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 @Table(name = "maintenance_record")
 public class MaintenanceRecord {
 
@@ -16,15 +25,22 @@ public class MaintenanceRecord {
     @GeneratedValue
     private UUID id;
 
-    @ManyToOne()
-    @JoinColumn(name = "vehicle_id")
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id",nullable = false)
     private Vehicle vehicle;
 
     @Column(nullable = false)
     @Schema(description = "mileage at which maintenance was done at")
-    private Integer changeAt;
+    private Integer mileage;
 
     @Column
     @Schema(description = "The date the maintenance was done on")
-    LocalDate doneTs;
+    private LocalDate date;
+
+    @Column(nullable = false)
+    @OneToMany(mappedBy = "maintenanceRecord",cascade = CascadeType.ALL)
+    private List<Part> parts;
+
+    @Column
+    private String description;
 }
---
+++ src/main/java/com/danialross/ServiceCycle/modules/MileageRecord/MileageRecord.java
@@ -0,0 +1,34 @@
+package com.danialross.ServiceCycle.modules.MileageRecord;
+
+import com.danialross.ServiceCycle.modules.vehicles.Vehicle;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@Table(name = "mileage_record")
+public class MileageRecord {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private Integer mileage;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne
+    @Column(nullable = false)
+    @JoinColumn(name = "vehicle_id",nullable = false)
+    private Vehicle vehicle;
+}
